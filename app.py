900767db36f1a5104c2c462a6c0b95e94870c665
MaciejWojt <maciej.wojtkowiak@student.put.poznan.pl>
2024-08-16 13:16:40 +0200

    added random images choice

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 import cv2
 import sys
-
+import random
 
 from augmenLib import (
     random_crop,
@@ -67,55 +67,64 @@ def main():
     match choice:
         case 0:
             print("WARNING: This method is not recommended for medical images, where every pixel is important")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 new_image = random_crop("photos/" + image_path)
                 cv2.imwrite(f"photos/{image_path}_cropped.jpg", new_image)
         
         case 1:
             print("WARNING: This method is not recommended for images with clear orientation, e.g. text image")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 new_image = random_rotate("photos/" + image_path)
                 cv2.imwrite(f"photos/{image_path}_rotated.jpg", new_image)
 
         case 2:
             print("WARNING: This method is not recommended for images with clear orientation, e.g. car image")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 new_image = flip_image("photos/" + image_path)
                 cv2.imwrite(f"photos/{image_path}_flipped.jpg", new_image)
         
         case 3:
             print("WARNING: This method is not recommended for images where size matter, e.g. medical image")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 new_image = random_resize("photos/" + image_path)
                 cv2.imwrite(f"photos/{image_path}_resised.jpg", new_image)
 
         case 4:
             print("WARNING: This method is not recommended for images where contrast matter, e.g. X-ray image")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 new_image = random_brightness("photos/" + image_path)
                 cv2.imwrite(f"photos/{image_path}_brightened.jpg", new_image)
 
         case 5:
             print("WARNING: This method is not recommended for images where contrast matter, e.g. X-ray image")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 new_image = adjust_contrast("photos/" + image_path)
                 cv2.imwrite(f"photos/{image_path}_adjust_contrast.jpg", new_image)
 
         case 6:
             print("WARNING: This method is not recommended for images where noise may lead to misdiagnosis, e.g. medical image")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 new_image = add_gaussian_noise("photos/" + image_path)
                 cv2.imwrite(f"photos/{image_path}_gaussian.jpg", new_image)
 
         case 7:
             print("WARNING: This method is not recommended for images where noise may lead to misdiagnosis, e.g. medical image")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 new_image = add_salt_and_pepper_noise_color("photos/" + image_path)
                 cv2.imwrite(f"photos/{image_path}_salt_and_pepper.jpg", new_image)
 
         case 8:
             print("WARNING: This method is not recommended for images where perspective is key, e.g. architecture image")
-            for image_path in image_names[:num_images]:
+            num_images_random = random.sample(image_names,num_images)
+            for image_path in num_images_random:
                 points = random_perspective_points("photos/" + image_path)
                 new_image = perspective_transform("photos/" + image_path, points)
                 cv2.imwrite(f"photos/{image_path}_perspective_transform.jpg", new_image)
